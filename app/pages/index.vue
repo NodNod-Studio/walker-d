8b48b41595043de3d4306d1ba3e873ef8fd6dc69
Daniele Polacco <polaccodaniele@gmail.com>
2026-09-22 10:55:18 +0200

    fix: add missing body tag in HTML head for proper structure

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -50,6 +50,7 @@ const head = `<!doctype html>
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
+    <body>
 `
 
 const tail = `
